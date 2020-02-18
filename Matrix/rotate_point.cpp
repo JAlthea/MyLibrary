675dca7832b62a1ec0dcb1c16af33ae22a4d81d3
Yongmin <althea_@naver.com>
2020-02-18 23:22:08 +0900

    Update rotate_point.cpp

--- Matrix/rotate_point.cpp
+++ Matrix/rotate_point.cpp
@@ -1,8 +1,22 @@
+#include <type_traits>
+/*
+enable : integer type, floating point number type
+*/
 template <typename T>
-pair<T, T> rotatePoint90D(const pair<T, T> &point)
+std::enable_if_t<std::is_floating_point_v<T>, std::pair<T, T>> rotatePoint90D(const std::pair<T, T> &point)
 {
 	int m[2][2] = { { 0, -1}, {1, 0} };
-	pair<T, T> rotatedPoint;
+	std::pair<T, T> rotatedPoint;
+	rotatedPoint.first = m[0][0] * point.first + m[0][1] * point.second;
+	rotatedPoint.second = m[1][0] * point.first + m[1][1] * point.second;
+	return rotatedPoint;
+}
+
+template <typename T>
+std::enable_if_t<std::is_integral_v<T>, std::pair<T, T>> rotatePoint90D(const std::pair<T, T> &point)
+{
+	int m[2][2] = { { 0, -1}, {1, 0} };
+	std::pair<T, T> rotatedPoint;
 	rotatedPoint.first = m[0][0] * point.first + m[0][1] * point.second;
 	rotatedPoint.second = m[1][0] * point.first + m[1][1] * point.second;
 	return rotatedPoint;
