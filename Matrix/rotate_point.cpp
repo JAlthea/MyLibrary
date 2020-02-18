d985c7e1b431a4bff2ea4a01557530929bbf4b97
Yongmin <althea_@naver.com>
2020-02-18 22:28:59 +0900

    Update rotate_point.cpp

--- Matrix/rotate_point.cpp
+++ Matrix/rotate_point.cpp
@@ -1,7 +1,7 @@
 template <typename T>
 pair<T, T> rotatePoint90D(const pair<T, T> &point)
 {
-	vector<vector<int>> m = { { 0, -1}, {1, 0} };
+	int m[2][2] = { { 0, -1}, {1, 0} };
 	pair<T, T> rotatedPoint;
 	rotatedPoint.first = m[0][0] * point.first + m[0][1] * point.second;
 	rotatedPoint.second = m[1][0] * point.first + m[1][1] * point.second;
