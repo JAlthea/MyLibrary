26326e5471f64cd5f3fb31106d20191f83d1d8e5
Yongmin <althea_@naver.com>
2020-02-18 23:25:42 +0900

    Update rotate_array.cpp

--- Matrix/rotate_array.cpp
+++ Matrix/rotate_array.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-using namespace std;
 
 template <typename T>
-vector<vector<T>> rotateArray(vector<vector<T>> &array)
+std::vector<std::vector<T>> rotateArray(std::vector<std::vector<T>> &array)
 {
 	int n = array.size();
 	int m = array[0].size();
-	vector<vector<T>> rotatedArray(m, vector<T>(n, 0));
+	std::vector<std::vector<T>> rotatedArray(m, std::vector<T>(n, 0));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			rotatedArray[j][n - 1 - i] = array[i][j];
