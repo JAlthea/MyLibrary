d3c19dc05f92bee3d6d4098ac73b117fa1c14c44
Yongmin <althea_@naver.com>
2020-02-18 17:02:17 +0900

    Update is_same.cpp

--- Compare/is_same.cpp
+++ Compare/is_same.cpp
@@ -11,11 +11,11 @@ bool isSame(const T a, const T b)
 template <>
 bool isSame<float>(const float a, const float b)
 {
-	return fabs(a - b) < numeric_limits<float>::epsilon()) ? true : false;
+	return fabs(a - b) < numeric_limits<float>::epsilon() ? true : false;
 }
 
 template <>
 bool isSame<double>(const double a, const double b)
 {
-	return fabs(a - b) < numeric_limits<double>::epsilon()) ? true : false;
+	return fabs(a - b) < numeric_limits<double>::epsilon() ? true : false;
 }
