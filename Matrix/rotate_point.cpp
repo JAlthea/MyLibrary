9208b19325494887435c4eebf4c70d28fc7cd7dc
Yongmin <althea_@naver.com>
2020-02-18 15:12:32 +0900

    Create rotate_point.cpp

---
+++ Matrix/rotate_point.cpp
@@ -0,0 +1,10 @@
+
+
+pair<int, int> rotatePoint90D(const pair<int, int> &point)
+{
+	vector<vector<int>> m = { { 0, -1}, {1, 0} };
+	pair<int, int> rotatedPoint;
+	rotatedPoint.first = m[0][0] * point.first + m[0][1] * point.second;
+	rotatedPoint.second = m[1][0] * point.first + m[1][1] * point.second;
+	return rotatedPoint;
+}
