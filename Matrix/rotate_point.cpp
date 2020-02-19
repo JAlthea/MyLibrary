462c0b7c978aef3a17bc3b3ccffdb57f60bec321
Yongmin <althea_@naver.com>
2020-02-19 19:38:39 +0900

    Update rotate_point.cpp

--- Matrix/rotate_point.cpp
+++ Matrix/rotate_point.cpp
@@ -4,7 +4,7 @@
 is_arithmetic : integer type, floating point number type
 */
 template <typename T>
-std::enable_if_t<std::is_arithmetic_v<T>, std::pair<T, T>> rotatePoint90D(const std::pair<T, T> &point)
+std::enable_if_t<std::is_arithmetic_v<T>, std::pair<T, T>> rotatePoint_90(const std::pair<T, T> &point)
 {
 	int m[2][2] = { { 0, -1}, {1, 0} };
 	std::pair<T, T> rotatedPoint;
