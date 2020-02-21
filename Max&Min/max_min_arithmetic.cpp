e1945d3af2ad86b919a387e283d5471392662fe8
Yongmin <althea_@naver.com>
2020-02-21 20:48:11 +0900

    Update and rename max_min.cpp to max_min_arithmetic.cpp

--- Max&Min/max_min_arithmetic.cpp
+++ Max&Min/max_min_arithmetic.cpp
@@ -1,25 +1,26 @@
+
 /* 2 arguments */
 template <typename T>
-T max(T a, T b)
+std::enable_if_t<std::is_arithmetic_v<T>, T> max(T a, T b)
 {
 	return a > b ? a : b;
 }
 
 template <typename T>
-T min(T a, T b)
+std::enable_if_t<std::is_arithmetic_v<T>, T> min(T a, T b)
 {
 	return a < b ? a : b;
 }
 
 /* 3 arguments */
 template <typename T>
-T max(T a, T b, T c)
+std::enable_if_t<std::is_arithmetic_v<T>, T> max(T a, T b, T c)
 {
 	return a < b ? b < c ? c : b : a > c ? a : c;
 }
 
 template <typename T>
-T min(T a, T b, T c)
+std::enable_if_t<std::is_arithmetic_v<T>, T> min(T a, T b, T c)
 {
 	return a > b ? b > c ? c : b : a < c ? a : c;
 }
