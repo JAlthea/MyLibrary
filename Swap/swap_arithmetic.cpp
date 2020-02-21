6e00384bfef046155dc3bb56e53a5ed43d9680d1
Yongmin <althea_@naver.com>
2020-02-21 20:54:26 +0900

    Update swap_arithmetic.cpp

--- Swap/swap_arithmetic.cpp
+++ Swap/swap_arithmetic.cpp
@@ -1,10 +1,11 @@
-/* XOR swap for Integer */
+/* XOR swap for Integer type */
 template <typename T>
 std::enable_if_t <std::is_integral_v<T>> swap(T &a, T &b)
 {
 	a ^= (b ^= (a ^= b));
 }
 
+/* Floating point number type */
 template <typename T>
 std::enable_if_t<std::is_floating_point_v<T>> swap(T &a, T &b)
 {
