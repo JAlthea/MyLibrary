66c68dd981707339034fe885bba89c4ba4d05a84
Yongmin <althea_@naver.com>
2020-10-07 21:20:28 +0900

    Create insertion_sort.cpp

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int> &arr)
+{
+    for (int i = 1; i < arr.size(); ++i)
+    {
+        int value = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > value)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = value;
+    }
+}
