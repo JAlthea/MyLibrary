2fafbe57ddc63d383ef5f9ac7c31f5b6a13f2f76
Yongmin <althea_@naver.com>
2020-10-08 02:12:53 +0900

    Update insertion_sort.cpp

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -30,10 +30,14 @@ int main()
 {
 	vector<int> arr = { 1, 3, 2, 5, 4 };
 	vector<double> arr2 = { 1.0, 3.1, 2.99, 5, 4 };
+	long long arr3[] = { 123124124124, 23412412414, 356124124124 };
 	insertionSort(arr.begin(), arr.end());
 	insertionSort(arr2.begin(), arr2.end());
+	insertionSort(arr3, arr3 + 3);
 	for (auto &a : arr) cout << a << ' ';
 	cout << '\n';
 	for (auto &a : arr2) cout << a << ' ';
+	cout << '\n';
+	for (auto &a : arr3) cout << a << ' ';
 }
 */
