573fe8ec4682e510980aa54ac301275a7e2ad123
Yongmin <althea_@naver.com>
2020-10-30 06:16:19 +0900

    Create choice_if_or_switch.cpp

---
+++ Optimization/choice_if_or_switch.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+//if branch >= 5, useful switch case 
+
+void solution(int value)
+{
+    if (value >= 10)
+    {
+    
+    }
+    else if (value >= 5)
+    {
+    
+    }
+    else if (value >= 1)
+    {
+    
+    }
+    else
+    {
+    
+    }
+}
+
+void solution(int value)
+{
+    switch (value)
+    {
+        case 10:
+        break;
+        case 7:
+        break;
+        case 4:
+        break;
+        case 1:
+        break;
+        default:
+        break;
+    }
+}
