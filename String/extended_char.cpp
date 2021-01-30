7b23db2b7d8cbff1ec3b88e87cb7a868632bdb2f
Yongmin <althea_@naver.com>
2021-01-30 23:37:40 +0900

    Update extended_char.cpp

--- String/extended_char.cpp
+++ String/extended_char.cpp
@@ -11,7 +11,7 @@ wchar_t : 2byte (0 ~ 65535)
 위의 4가지의 경우는 완전히 같지 않음을 유의해야 한다.
 char와 signed char의 경우, 같은 의미로 쓰일 수는 있으나 엄밀하게는 다르다고 생각하자.
 
-ASCII의 0 ~ 127은 현재 사용하는 문자, 128 ~ 255는 예전에 사용했던 문자에 해당한다.
+ASCII의 0 ~ 127은 현재 사용하는 문자, 128 ~ 255는 예전에 사용했던 특수문자에 해당한다.
 128번 부터는 현재 표준이 아니고, 운영체제마다 호환이 되지 않는다.
 
 Java와 같은 다른 OOP에서 문자형으로 사용하는 Unicode에 해당하는 문자를 다루기 위해서는 wchar_t 를 사용하자.
@@ -19,7 +19,7 @@ Java와 같은 다른 OOP에서 문자형으로 사용하는 Unicode에 해당�
 [참고]
 https://stackoverflow.com/questions/75191/what-is-an-unsigned-char
 https://shaeod.tistory.com/228
-
+https://blog.naver.com/lhk0523/140141011673
 */
 
 int main()
@@ -27,7 +27,10 @@ int main()
 	setlocale(LC_ALL, "KO");
 	wchar_t c = L'가';
 	wchar_t d = L'나';
+	wchar_t s[] = L"다 함께";
 	wcout << c << '\n';
 	wprintf(L"%c\n", d);
-	putwchar(L'다');
+	wprintf(L"%s\n", s);
+	cout << sizeof(d) << '\n';
+	cout << sizeof(s) << '\n';
 }
