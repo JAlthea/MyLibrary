392865ed3050e3f4d9f8f13e0e360f4e9fce2c68
Yongmin <althea_@naver.com>
2021-02-08 12:10:13 +0900

    Create palindrome.cpp

---
+++ String/palindrome.cpp
@@ -0,0 +1,61 @@
+
+bool isPalindrome(int n)
+{
+	int tmp = n;
+	int cmp = 0;
+	while (tmp)
+	{
+		cmp *= 10;
+		cmp += tmp % 10;
+		tmp /= 10; 
+	}
+
+	return n == cmp;
+}
+
+void makePalindrome(int length)
+{
+	vector<string> ret;
+	vector<int> palins;
+    
+	int left_right = 0;
+	while (left_right <= 9)
+	{
+		ret.push_back(to_string(left_right));
+		++left_right;
+	}
+  
+	left_right = 0;
+	while (left_right <= 9)
+	{
+		ret.push_back(to_string(left_right) + to_string(left_right));
+		++left_right;
+	}
+
+	int prevSize = 0;
+	int nowSize = ret.size();
+	while (ret.back().size() < length)
+	{
+		for (int i = prevSize; i < nowSize; ++i)
+		{
+			left_right = 0;
+			while (left_right <= 9)
+			{
+				ret.push_back(to_string(left_right) + ret[i] + to_string(left_right));
+				++left_right;
+			}
+		}
+
+		prevSize = nowSize;
+		nowSize = ret.size();
+	}
+
+	for (int i = 0; i < ret.size(); ++i)
+	{
+		if (ret[i].front() == '0') continue;
+		palins.push_back(stoi(ret[i]));
+	}
+
+	sort(palins.begin(), palins.end());
+	return palins;
+}
