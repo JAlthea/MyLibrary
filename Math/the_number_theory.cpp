2f112fa75932193fc4174271dd9122df02876f4b
Yongmin <althea_@naver.com>
2021-02-09 13:42:14 +0900

    Create the_number_theory.cpp

---
+++ Math/the_number_theory.cpp
@@ -0,0 +1,15 @@
+/* Integer type : char, unsigned char, wchar_t, short, int, long, long long, ...
+
+*/
+
+
+template <typename T>
+std::enable_if_t<std::is_integral_v<T>, T> GCD(T a, T b)
+{
+	return b ? GCD(b, a % b) : a;
+}
+
+std::enable_if_t<std::is_integral_v<T>, T> LCM(T a, T b)
+{
+    return a * b / GCD(a, b);
+}
