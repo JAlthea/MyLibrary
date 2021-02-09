1bd5a0ce9e262f5bcd74649fc16e93151b20c2a2
Yongmin <althea_@naver.com>
2021-02-09 13:42:31 +0900

    Update the_number_theory.cpp

--- Math/the_number_theory.cpp
+++ Math/the_number_theory.cpp
@@ -6,7 +6,7 @@
 template <typename T>
 std::enable_if_t<std::is_integral_v<T>, T> GCD(T a, T b)
 {
-	return b ? GCD(b, a % b) : a;
+    return b ? GCD(b, a % b) : a;
 }
 
 std::enable_if_t<std::is_integral_v<T>, T> LCM(T a, T b)
