5eccd6735c35b6a3b6f9762760391658a3158078
Yongmin Jang <althea_@naver.com>
2021-03-31 01:43:12 +0900

    Update JAlthea_hash.cpp

--- Hash/JAlthea_hash.cpp
+++ Hash/JAlthea_hash.cpp
@@ -21,32 +21,32 @@ const unsigned M = 1234567891;
 
 
 unsigned hashFunction(string & s) {
-	unsigned ret = 0;
-	unsigned long long R_pow = 1;
-	for (auto & c : s) {
-		ret += (c - 96) * R_pow % M;
-		R_pow = R_pow * R % M;
-	}
+    unsigned ret = 0;
+    unsigned long long R_pow = 1;
+    for (auto & c : s) {
+        ret += (c - 96) * R_pow % M;
+        R_pow = R_pow * R % M;
+    }
 
-	return ret;
+    return ret;
 }
 
 template <typename T>
 enable_if_t<is_integral_v<T>, unsigned> hashFunction(vector<T> & seq) {
-	unsigned ret = 0;
-	unsigned long long R_pow = 1;
-	for (auto & n : s) {
-		ret += n * R_pow % M;
-		R_pow = R_pow * R % M;
-	}
+    unsigned ret = 0;
+    unsigned long long R_pow = 1;
+    for (auto & n : s) {
+        ret += n * R_pow % M;
+        R_pow = R_pow * R % M;
+    }
     
     return ret;
 }
 
 template <typename T>
 enable_if_t<is_integral_v<T>, unsigned> hashFunction(T & num) {
-	unsigned ret = 0;
-	unsigned long long R_pow = 1;
+    unsigned ret = 0;
+    unsigned long long R_pow = 1;
     
     T ret_num = num;
     while (ret_num) {
@@ -63,17 +63,17 @@ enable_if_t<is_integral_v<T>, unsigned> hashFunction(T & num) {
 
 
 void TestHashCollision() {
-	//write struct type hashFunction. i'm sleepy.
+    //write struct type hashFunction. i'm sleepy.
     unordered_map<string, int, hashFunction()> hashMap;
     vector<int> d = { 1, 1234, 45, 7, 11, 199, 22, 19, 101, 55555555, 77345, 918 };
-	string s1 = "aabbccz";
-	string s2 = "ababzcc";
-	string s3 = "ababzcd";
+    string s1 = "aabbccz";
+    string s2 = "ababzcc";
+    string s3 = "ababzcd";
 	
-	hashMap.insert(s1);
-	hashMap.insert(s2);
-	hashMap.insert(s3);
-	cout << hashMap[s1] << '\n';
-	cout << hashMap[s2] << '\n';
-	cout << hashMap[s3] << '\n';
+    hashMap.insert(s1);
+    hashMap.insert(s2);
+    hashMap.insert(s3);
+    cout << hashMap[s1] << '\n';
+    cout << hashMap[s2] << '\n';
+    cout << hashMap[s3] << '\n';
 }
