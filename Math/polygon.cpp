1f758777e9efb3072308d1f8ad4ace2f6b462db9
Yongmin Jang <althea_@naver.com>
2021-04-19 14:38:45 +0900

    Update and rename polygonArea.cpp to polygon.cpp

--- Math/polygon.cpp
+++ Math/polygon.cpp
@@ -6,7 +6,7 @@ using namespace std;
 double getPolygonArea(vector<pair<long long, long long>> & points) {
     long long area = 0;
     for (int i = 0; i < n; ++i) {
-        area += (arr[i].first + arr[(i + 1) % n].first) * (arr[i].second - arr[(i + 1) % n].second);
+        area += (points[i].first + points[(i + 1) % n].first) * (points[i].second - points[(i + 1) % n].second);
     }
     return (double)abs(area) / 2;
 }
