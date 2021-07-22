0840d97b9a362a1edd579cbfc799267e290ad12b
Yongmin Jang <althea_@naver.com>
2021-07-23 00:05:10 +0900

    Update choice_if_or_switch.cpp

--- Optimization/choice_if_or_switch.cpp
+++ Optimization/choice_if_or_switch.cpp
@@ -3,39 +3,40 @@ using namespace std;
 
 //if branch >= 5, useful switch case 
 
-void solution(int value)
-{
-    if (value >= 10)
-    {
+void solution(int value) {
+    if (value == 10) {
     
     }
-    else if (value >= 5)
-    {
+    else if (value == 7) {
     
     }
-    else if (value >= 1)
-    {
+    else if (value == 4) {
     
     }
-    else
-    {
+    else if (value == 1) {
     
+    }
+    else {
+
     }
 }
 
-void solution(int value)
-{
-    switch (value)
-    {
+void solution(int value) {
+    switch (value) {
         case 10:
+
         break;
         case 7:
+
         break;
         case 4:
+
         break;
         case 1:
+
         break;
         default:
+        
         break;
     }
 }
