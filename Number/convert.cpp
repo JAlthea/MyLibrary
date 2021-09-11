31bf5b05bcb03306ff7e8f30f4c97fcc3d8cc1ee
Yongmin Jang <althea_@naver.com>
2021-09-11 17:18:03 +0900

    Create convert.cpp

---
+++ Number/convert.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+//10진수(n) -> k진수
+string convertN(int n, int k) {
+    string result;
+    char intToChar[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    
+    while (n / k) {
+        result = intToChar[n % k] + result;
+        n /= k;
+    }
+    result = intToChar[n % k] + result;
+    
+    return result;
+}
