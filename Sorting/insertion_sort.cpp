c8385db173a83e16f79c1851a0be3492df3f4bbc
Yongmin Jang <althea_@naver.com>
2021-10-30 17:38:09 +0900

    Update insertion_sort.cpp

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -7,17 +7,14 @@ example of actual use : C++ / std::sort()
 */
 
 template <class RandomIt>
-inline void insertionSort(RandomIt first, RandomIt last)
-{
+inline void insertionSort(RandomIt first, RandomIt last) {
 	_Adl_verify_range(first, last);
 	const auto UFirst = _Get_unwrapped(first);
 	const auto ULast = _Get_unwrapped(last);
-	for (auto i = UFirst + 1; i < ULast; ++i)
-	{
+	for (auto i = UFirst + 1; i < ULast; ++i) {
 		auto value = *i;
 		auto j = i - 1;
-		while (j >= 0 && *j > value)
-		{
+		while (j >= 0 && *j > value) {
 			*(j + 1) = *j;
 			j--;
 		}
@@ -27,8 +24,7 @@ inline void insertionSort(RandomIt first, RandomIt last)
 
 /*
 //test
-int main()
-{
+int main() {
 	vector<int> arr = { 1, 3, 2, 5, 4 };
 	vector<double> arr2 = { 1.0, 3.1, 2.99, 5, 4 };
 	long long arr3[] = { 123124124124, 23412412414, 356124124124 };
