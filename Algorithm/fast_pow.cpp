654f774431a1d8439601d75a97145429ad79cd5a
Yongmin Jang <althea_@naver.com>
2021-12-22 22:25:56 +0900

    Update fast_pow.cpp

--- Algorithm/fast_pow.cpp
+++ Algorithm/fast_pow.cpp
@@ -1,9 +1,9 @@
-long long fast_pow(int x, int n) {
+long long fastPow(int x, int n) {
     if (n == 1)
         return x;
   
     if (n % 2) 
-        return x * fast_pow(x, n - 1);
+        return x * fastPow(x, n - 1);
 	
     long long ret = f(x, n / 2);
     return ret * ret;
