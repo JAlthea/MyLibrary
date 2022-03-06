01fddce1b6b3b9fcdb2c217df09473d6224083ad
Yongmin Jang <althea_@naver.com>
2022-03-06 15:47:17 +0900

    Update stringlib.cpp

--- String/stringlib.cpp
+++ String/stringlib.cpp
@@ -6,3 +6,24 @@ vector<string> split(const string &s, char deli) {
 		result.push_back(nowString);
 	return result;
 }
+
+int timeToInt(const string &stringTimeFormat) {
+    vector<string> s = split(stringTimeFormat, ':');
+    if (s.empty()) 
+        return -1;
+    
+    // hh
+    if (s.size() == 1) 
+        return stoi(s[0]);
+    
+    // hh:mm
+    if (s.size() == 2) 
+        return 60 * stoi(s[0]) + stoi(s[1]);
+    
+    // hh:mm:ss
+    if (s.size() == 3) 
+        return 3600 * stoi(s[0]) + 60 * stoi(s[1]) + stoi(s[2]);
+    
+    // not supported
+    return -1;
+}
